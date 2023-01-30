cf62a10415eb921681ff060de4b9db11fae49d36
Juan <juan.giana8589@gmail.com>
2023-01-30 08:49:49 -0600

    added note

--- distance.js
+++ distance.js
@@ -2,7 +2,7 @@ let x1 = 1;
 let y1 = 3;
 let x2 = 7;
 let y2 = 6;
-
+// this code was copied not my code, i understand problem now. 
 function getDistance(x1, y1, x2, y2){
     let y = x2 - x1;
     let x = y2 - y1;    
